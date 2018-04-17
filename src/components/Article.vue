7ae6c580dcdbe4c8922b8f779d728b61236f5c5f
Amy Carrigan - US <acarriga@worldvision.org>
2018-04-17 16:35:35 -0500

    adding new components

---
+++ src/components/Article.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="article">
+    <h1 class="article__title">{{ article.title }}</h1>
+    <router-link to="/blog">Back</router-link>
+    <div class="article__body" v-html="article.body"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Article',
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      article: {
+        title: this.id,
+        body: '<h2>Testing</h2><p>Ok, let\'s do more now!</p>',
+      },
+    };
+  },
+};
+</script>
+
